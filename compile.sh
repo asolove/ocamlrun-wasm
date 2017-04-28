f70bd77715aea10a7cd7d0ad25bb39d22217829b
asolove <asolove@gmail.com>
2017-04-28 11:33:05 -0600

    Upgrade ocaml. Steal an example about scheduling with effects.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./ocaml/build/bin/ocamlrun ./ocaml/build/bin/ocamlc -o build/example example.ml 
+./ocaml/build/bin/ocamlrun ./ocaml/build/bin/ocamlc -o build/example sched.mli sched.ml example.ml 
 emcc -s WASM=1 -s TOTAL_MEMORY=1342177280 -O2 build/ocamlrun.o -o build/ocamlrun.js --preload-file build/example
--- example.ml
+++ example.ml
@@ -1,20 +1,30 @@
-effect E : int 
-effect F : int 
+(* Demonstrate the concurrent scheduler
+  ------------------------------------
+   Spawn binary tree of tasks in depth-first order
+       ************
+        Fiber tree
+       ************
+             0
+           /  \
+          1    2
+         / \  / \
+        3   4 5  6
+*)
 
-let main () = 
-  try 
-    print_endline "perform E";
-    let v1 = perform E in
-    print_endline "perform F";
-    let v2 = perform F in
-    v1 + v2
-  with
-  | effect E k -> 
-      print_endline "handle E";
-      continue k 1
-  | effect F k ->
-      print_endline "handle F";
-      continue k 2
+let log = Printf.printf
 
-let result = main ()
-let _ = Printf.printf "main returned: %d\n%!" result
+let rec f id depth =
+  log "Starting number %i\n%!" id;
+  if depth > 0 then begin
+    log "Forking number %i\n%!" (id * 2 + 1);
+    Sched.fork (fun () -> f (id * 2 + 1) (depth - 1));
+    log "Forking number %i\n%!" (id * 2 + 2);
+    Sched.fork (fun () -> f (id * 2 + 2) (depth - 1))
+  end else begin
+    log "Yielding in number %i\n%!" id;
+    Sched.yield ();
+    log "Resumed number %i\n%!" id;
+  end;
+  log "Finishing number %i\n%!" id
+
+let () = Sched.run (fun () -> f 0 2)
